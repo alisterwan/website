f0e9a69f990125d7185880f5b0e182933552ef06
Nicolas Stepien <mayhemydg@gmail.com>
2011-05-03 14:04:03 +0200

    Fichiers de bases.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Home page</title>
+		<meta name="description" content="Projet web">
+		<meta name="author" content="Alister & Mayhem">
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+	<body>
+	</body>
+</html>
