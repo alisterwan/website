ecafbd918f96bff6c9c4985642b845b1d5e7d9d8
Nicolas Stepien <mayhemydg@gmail.com>
2011-05-06 15:53:40 +0200

    Cleanup

--- registration.php
+++ registration.php
@@ -1,47 +1,50 @@
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-<title>Registration new member</title>
-<meta name="generator" content="Bluefish 2.0.1" >
-<meta name="author" content="john wan kut kai" >
-<meta name="date" content="2011-05-05T19:51:10+0200" >
-<meta name="copyright" content="">
-<meta name="keywords" content="">
-<meta name="description" content="">
-<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
-<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8">
-<meta http-equiv="content-style-type" content="text/css">
-<meta http-equiv="expires" content="0">
-</head>
-<body>
-Pour vous inscrire dans ce club, nous avons besoin de quelques renseignements. 
-Preparez un nom d'usage pour le club et un mot de passe de plus de 8 caracteres
-dont vous vous souviendrez facilement.
-<form action="inscription_p2.php" method="post" name="page_1">
-<table cellspacing="2" cellpadding="2" border="0" >
-<tr>
-<td>Votre nom:</td>
-<td><input type="text" name="name"></td>
-</tr>
-<tr>
-<td>Votre adresse:</td>
-<td><input type="text" name="address"></td>
-</tr>
-<tr>
-<td>nom d'utilisateur:</td>
-<td><input type="text" name="username"></td>
-</tr>
-<tr>
-<td>mot de passe:</td>
-<td><input type="password" name="password"></td>
-</tr>
-<tr>
-<td>Numero de carte bancaire:</td>
-<td><input type="text" name="num_card"></td>
-</tr>
-</table>
-<input type="submit" name="Je m'inscris">
- </form> 
-</body>
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Registration</title>
+		<meta name="description" content="Projet web">
+		<meta name="author" content="Alister & Mayhem">
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+	<body>
+		<?php include './header.php' ?>
+
+		<div id="body">
+			<?php include './navigation.php' ?>
+			<div id="content">
+				<p>Pour vous inscrire dans ce club, nous avons besoin de quelques renseignements. Preparez un nom d'usage pour le club et un mot de passe de plus de 8 caracteres dont vous vous souviendrez facilement.</p>
+				<form action="inscription.php" method="post" name="registration">
+					<table>
+						<tr>
+							<td>Votre nom:</td>
+							<td><input type="text" name="name"></td>
+						</tr>
+						<tr>
+							<td>Votre adresse:</td>
+							<td><input type="text" name="address"></td>
+						</tr>
+						<tr>
+							<td>Nom d'utilisateur:</td>
+							<td><input type="text" name="username"></td>
+						</tr>
+						<tr>
+							<td>Mot de passe:</td>
+							<td><input type="password" name="password"></td>
+						</tr>
+						<tr>
+							<td>Numero de carte bancaire:</td>
+							<td><input type="text" name="num_card"></td>
+						</tr>
+						<tr>
+							<td><input type="submit" name="proceed" value="submit"></td>
+						<tr>
+					</table>
+				</form>
+			</div>
+		</div>
+
+		<?php include './footer.php' ?>
+	</body>
 </html>
