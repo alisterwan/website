ab4eb59e942581e9934f21d34133b3153b31e52f
Nicolas Stepien <mayhemydg@gmail.com>
2011-05-08 21:35:18 +0200

    Repositionne le logo

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div id="header">
+	<a href="./index.php"><img src="laptop.png" height="125px"></a>
 	<span id="log">Welcome. <a href="./login.php">Log in</a> or <a href="./registration.php">register</a>.</span>
 	<a href="./cart.php" id="cart">Cart</a>
-	<a href="./index.php"><img src="laptop.png" width="140" height="110"></img></a>
 </div>
