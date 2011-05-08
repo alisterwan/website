b9fd2c6f5b56b31bd54418e8d275a28f6c763816
Nicolas Stepien <mayhemydg@gmail.com>
2011-05-08 22:15:25 +0200

    Oops

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div id="header">
-	<a href="./index.php"><img src="laptop.png" height="125px"></a>
+	<a href="./index.php"><img src="laptop.png"></a>
 	<span id="log">Welcome. <a href="./login.php">Log in</a> or <a href="./registration.php">register</a>.</span>
 	<a href="./cart.php" id="cart">Cart</a>
 </div>
