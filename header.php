4bb714febd9ef02e4b11aa4e05ebedfb2c456efb
John Wan <alisterwan@gmail.com>
2011-05-08 17:18:20 +0200

    header with logo

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
 <div id="header">
 	<span id="log">Welcome. <a href="./login.php">Log in</a> or <a href="./registration.php">register</a>.</span>
 	<a href="./cart.php" id="cart">Cart</a>
+	<a href="./index.php"><img src="laptop.png" width="140" height="110"></img></a>
 </div>
