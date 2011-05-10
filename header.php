c573dfa57afbb2549aa4c36c7e75235a1a390d30
Nicolas Stepien <mayhemydg@gmail.com>
2011-05-10 14:12:06 +0200

    Cart dans header

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div id="header">
-	<a href="./index.php"><img src="laptop.png"></a>
+	<a href="./index.php"><img src="laptop.png" id="logo"></a>
 	<span id="log">Welcome. <a href="./login.php">Log in</a> or <a href="./registration.php">register</a>.</span>
-	<a href="./cart.php" id="cart">Cart</a>
+	<a href="./cart.php" id="cart">Cart <img src="cart.png"></a>
 </div>
