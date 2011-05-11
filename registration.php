4d144fb10a7eccb931aa5baed0d055a72fd38671
Nicolas Stepien <mayhemydg@gmail.com>
2011-05-11 18:27:21 +0200

    Fix form conditions, clean/indentation.

--- registration.php
+++ registration.php
@@ -12,14 +12,14 @@
 		<?php include './header.php' ?>
 
 		<div id="body">
-		
+
 			<?php include './navigation.php' ?>
 			<div id="content">
+				<?php
+					function printForm($firstname,$surname,$address,$city,$country,$user,$email) {
+						echo "
 				<p>In order to register, please provide necessary information. You username will be used in order to log in. Your password must be at least 8 characters long.</p>
-				
-			<?php
-				function printForm($firstname,$surname,$address,$city,$country,$user,$email) {			
-					echo "<form action='registration.php' method='post' name='registration'>
+				<form action='registration.php' method='post' name='registration'>
 					<table>
 						<tr>
 							<td>Firstname</td>
@@ -28,7 +28,7 @@
 						<tr>
 							<td>Surname</td>
 							<td><input type='text' name='surname' value='$surname'></td>
-						</tr>						
+						</tr>
 						<tr>
 							<td>Address</td>
 							<td><input type='text' name='address' value='$address'></td>
@@ -43,7 +43,7 @@
 						</tr>
 						<tr>
 							<td>Username</td>
-							<td><input type='text' name='username' value='$username'></td>
+							<td><input type='text' name='username' value='$user'></td>
 						</tr>
 						<tr>
 							<td>Password</td>
@@ -62,51 +62,48 @@
 						<tr>
 					</table>
 				</form>";
-			}
-			
-			if ($_POST["firstname"] && $_POST["surname"]) {
-						
-			$firstname= $_POST["firstname"];
-			$surname  = $_POST["surname"];
-			$address  = $_POST["address"];
-			$city     = $_POST["city"];
-			$country  = $_POST["country"];
-			$user     = $_POST["username"];
-			$pass     = $_POST["password"];
-			$email    = $_POST["email"];
-						
-			if ($pass && strlen($pass)<9) {
-				echo "Password invalid ";
-				printForm($firstname,$surname,$address,$city,$country,$username);return;
-			}
+					}
 
-			if ($_POST["password"] != $_POST["passcheck"]) {
-					echo "Wrong password.";
-					exit;
-		
-		
-		
-			}
-			
-			//Connexion à la base de donnée
-			$conn = new PDO("pgsql:host=sqletud.univ-mlv.fr;dbname=jwankutk_db","jwankutk","Tqeouoe8");
-			if (!$conn) {
-				echo "Connexion error.";
-				exit;
-			}
-							
-			
-			// Ajout d'un nouveau client dans la base de donnée
-			$result = $conn -> query("INSERT INTO customers VALUES ('$firstname','$surname','$address','$city','$country','$user','$pass','$email')");
-			if(!$result) {
-				echo "Query error.";
-				exit;
-			}
-			else echo "You have successfully registered.";
-			
-	
+					if ($_POST["firstname"] && $_POST["surname"]) {
+
+						$firstname= $_POST["firstname"];
+						$surname  = $_POST["surname"];
+						$address  = $_POST["address"];
+						$city     = $_POST["city"];
+						$country  = $_POST["country"];
+						$user     = $_POST["username"];
+						$pass     = $_POST["password"];
+						$email    = $_POST["email"];
+
+						if ($pass && strlen($pass)<8 || $_POST["password"] != $_POST["passcheck"]) {
+							echo "Password invalid";
+							printForm($firstname,$surname,$address,$city,$country,$user,$email);
+							exit;
+						}
+
+						//Connexion à la base de donnée
+						$conn = new PDO("pgsql:host=sqletud.univ-mlv.fr;dbname=jwankutk_db","jwankutk","Tqeouoe8");
+						if (!$conn) {
+							echo "Connexion error.";
+							printForm($firstname,$surname,$address,$city,$country,$user,$email);
+							exit;
+						}
+
+
+						// Ajout d'un nouveau client dans la base de donnée
+						$result = $conn -> query("INSERT INTO customers VALUES ('$firstname','$surname','$address','$city','$country','$user','$pass','$email')");
+						if(!$result) {
+							echo "Query error.";
+							printForm($firstname,$surname,$address,$city,$country,$user,$email);
+							exit;
+						}
+						else
+							echo "You have successfully registered.";
+
+
 
 			//Envoie du mail de confirmation de l'inscription
+			/*
 			$headers ='From: "laptopmlv"<laptopmlv@gmail.com>'."\n";
 			$headers .='Content-Type: text/plain; charset="iso-8859-1"'."\n";
 			$headers .='Content-Transfer-Encoding: 8bit';
@@ -114,14 +111,13 @@
 
 			if(mail($to,"Registration to LMLV","You have been correctly registered.", $headers)) {
 				echo '<br>An email of confirmation has been sent.<br>';
-				
+
 			}
-			else {
+			else
 				echo 'The email has not been sent.';
-			}
-			}
-		
-		?>
+			*/
+					}
+				?>
 			</div>
 		</div>
 
