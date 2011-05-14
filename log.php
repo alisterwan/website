9cc3a1b8a95b6a49ead1c4d6612145355ab9a194
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-14 13:52:36 +0200

    authentification successful

--- log.php
+++ log.php
@@ -5,27 +5,25 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>Login</title>
 		<meta name="description" content="Projet web">
-		<meta name="author" content="john wan kut kai" >
+		<meta name="author" content="john" >
 		<link rel="stylesheet" href="stylesheet.css">
 	</head>
 	<body>
 <?php
-$user = $_POST["username"];
-$pass = $_POST["password"];
+$user= $_POST["username"];
+$pass= $_POST["password"];
 
 	//Connexion à la base de donnée
-			$conn = new PDO("pgsql:host=sqletud.univ-mlv.fr;dbname=jwankutk_db","jwankutk","Tqeouoe8");
-			if (!$conn) {
-				echo "Connexion error.";
-				exit;
-			}
+		$conn = "host=sqletud.univ-mlv.fr port=5432 dbname=jwankutk_db user=jwankutk password=Tqeouoe8";
+		$connect=pg_connect($conn);
+		
 //Verification du client dans la base de donnée
-$result = $conn -> query("SELECT (username,password) from customers where username='$user' and password='$pass'"); 
-	
-			if(!$result) {
-				echo "Query error";
-			} 
-else echo "Authenfication reussie.";
+$result = pg_query($connect,"SELECT (username,password) from customers where username='$user' and password='$pass'"); 
+$count=pg_num_rows($result);
+
+if($count!=1){echo "Please retry to log in.Click <a href=./login.php>here</a>";}
+if($count==1){echo "You have successfully logged in. Click <a href='./index.php'>here</a> to continue.";}
+
 ?>
 	</body>
 </html>
