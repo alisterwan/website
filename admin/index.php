0de3765618b4b16848be52540391d68b8422e2f9
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-16 11:06:47 +0200

    category to choose

--- admin/index.php
+++ admin/index.php
@@ -6,18 +6,27 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<title>Administration panel</title>
 		<meta name="description" content="Projet web">
-		<meta name="author" content="Alister & Mayhem">
+		<meta name="author" content="Alister & Mayhem" >
 		<link rel="stylesheet" href="stylesheet.css">
 	</head>
 	<body>
-
-		<?php
+		<?php   
 			if ($_SESSION[masterpass] == laptopmlv)
-				echo "Please choose a category to upload your product.";
+				
+				echo "";
+				echo "Welcome webmaster.";
+				echo "<br/>";
+				echo "Choose a category to upload.";
+				echo "<br/>";
 				echo "<a href='./productpanel.php'><u>Laptop</u></a>";
 				echo "<br/>";
 				echo "<a href><u>Accessories </u></a>";
-		?>
+				echo"<br />";
 
+		?>
+<?php 
+echo "<a href='./login.php'>Log out</a>";
+?>
+		
 	</body>
 </html>
