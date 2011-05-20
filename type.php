ef13f3cab61896190a23b105aa0f3a8e990263ab
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-20 10:28:25 +0200

    update type

--- type.php
+++ type.php
@@ -23,68 +23,35 @@
 						$result = pg_query($conn,"SELECT id_laptop, model FROM laptop WHERE type='$type' and brand='$brand'");
 
 					
-					
-						while ($i = pg_fetch_row($result))
+						if (pg_num_rows($result)) {
 							echo "
+				<div class='type'>
+					<div class='title'>$brand</div>";
+							while ($i = pg_fetch_row($result))
+								echo "
 				<a href='./laptop.php?id=$i[0]'><div class='unit'>
 					<div><img src='./$brand/$type/$i[1].png'></div>
 					<div>$i[1]</div>
 				</div></a>";
+							echo "
+					<br clear='left'>
+				</div>";
+						}
 					}
 
 					$type = $_GET[type];
 					echo "<img src='./typelogos/$type.png'>";
 				?>
 
-				<div class="type">
-					<div class="title">Acer</div>
 					<?php
 						printLaptop($type,Acer);
-					?>
-					<br clear="left">
-				</div>
-				<div class="type">
-					<div class="title">Asus</div>
-					<?php
 						printLaptop($type,Asus);
-					?>
-					<br clear="left">
-				</div>
-				<div class="type">
-					<div class="title">Apple</div>
-					<?php
 						printLaptop($type,Apple);
-					?>
-					<br clear="left">
-				</div>
-				<div class="type">
-					<div class="title">Dell</div>
-					<?php
 						printLaptop($type,Dell);
-					?>
-					<br clear="left">
-				</div>
-				<div class="type">
-					<div class="title">Hewlett Pacard</div>
-					<?php
 						printLaptop($type,HP);
-					?>
-					<br clear="left">
-				</div>
-				<div class="type">
-					<div class="title">Toshiba</div>
-					<?php
 						printLaptop($type,Toshiba);
-					?>
-					<br clear="left">
-				</div>
-				<div class="type">
-					<div class="title">Samsung</div>
-					<?php
 						printLaptop($type,Samsung);
 					?>
-					<br clear="left">
-				</div>
 
 			</div>
 		</div>
