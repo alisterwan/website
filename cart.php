473a5c98235a0d70ce5b2275b0d6c3767306f3c7
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-21 14:54:30 +0200

    add on to the cart

--- cart.php
+++ cart.php
@@ -31,6 +31,62 @@ include_once("cart_function.php");
 
 
 	<?php
+	
+
+$error = false;
+
+$action = (isset($_POST['action'])? $_POST['action']:  (isset($_GET['action'])? $_GET['action']:null )) ;
+if($action != null)
+{
+   if(!in_array($action,array('add', 'delete', 'refresh')))
+   
+   $error=true;
+
+   //récuperation des variables en POST ou GET
+   $l = (isset($_POST['l'])? $_POST['l']:  (isset($_GET['l'])? $_GET['l']:null )) ;
+   $p = (isset($_POST['p'])? $_POST['p']:  (isset($_GET['p'])? $_GET['p']:null )) ;
+   $q = (isset($_POST['q'])? $_POST['q']:  (isset($_GET['q'])? $_GET['q']:null )) ;
+
+   //Suppression des espaces verticaux
+   $l = preg_replace('#\v#', '',$l);
+   //On verifie que $p soit un float
+   $p = floatval($p);
+
+   //On traite $q qui peut etre un entier simple ou un tableau d'entier
+    
+   if (is_array($q)){
+      $quantity = array();
+      $i=0;
+      foreach ($q as $content){
+         $quantity[$i++] = intval($content);
+      }
+   }
+   else
+   $q = intval($q);
+    
+}
+
+if (!$error){
+   switch($action){
+      Case "add":
+         addLaptop($l,$q,$p);
+         break;
+
+      Case "delete":
+         deleteProduct($l);
+         break;
+
+      Case "refresh" :
+         for ($i = 0 ; $i < count($quantity) ; $i++)
+         {
+            modifyQtyProduct($_SESSION['cart']['id_laptop'][$i],round($quantity[$i]));
+         }
+         break;
+
+      Default:
+         break;
+   }
+}	
 	if (creationcart())
 	{
 		$nbProduct=count($_SESSION['cart']['id_laptop']);
@@ -61,6 +117,7 @@ include_once("cart_function.php");
 		}
 	}
 	?>
+	
 </table>
 </form>
 </div>
