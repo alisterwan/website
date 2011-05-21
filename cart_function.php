dab1f20e382781eb114fbca30d715fdef5005ce5
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-21 15:38:32 +0200

    modif on id_laptop

--- cart_function.php
+++ cart_function.php
@@ -4,7 +4,7 @@
 function creationcart(){
    if (!isset($_SESSION['cart'])){
       $_SESSION['cart']=array();
-      $_SESSION['cart']['id_laptop'] = array();
+      $_SESSION['cart']['model'] = array();
       $_SESSION['cart']['quantity'] = array();
       $_SESSION['cart']['price'] = array();
       $_SESSION['cart']['lock'] = false;
@@ -24,7 +24,7 @@ function isLock(){
 function countProduct()
 {
    if (isset($_SESSION['cart']))
-   return count($_SESSION['cart']['id_laptop']);
+   return count($_SESSION['cart']['model']);
    else
    return 0;
 
@@ -36,13 +36,13 @@ function deletecart(){
 }
 
 //fonction qui ajoute un laptop au panier
-function addLaptop($id_laptop,$quantity,$price){
+function addLaptop($model,$quantity,$price){
 
    //Si le cart existe
    if (creationcart() && !isLock())
    {
       //Si le produit existe déjà on ajoute seulement la quantité
-      $searchProduct = array_search($id_laptop, $_SESSION['cart']['id_laptop']);
+      $searchProduct = array_search($model, $_SESSION['cart']['model']);
 
       if ($searchProduct !== false)
       {
@@ -51,7 +51,7 @@ function addLaptop($id_laptop,$quantity,$price){
       else
       {
          //Sinon on ajoute le produit
-         array_push( $_SESSION['cart']['id_laptop'],$id_laptop);
+         array_push( $_SESSION['cart']['model'],$model);
          array_push( $_SESSION['cart']['quantity'],$quantity);
          array_push( $_SESSION['cart']['price'],$price);
       }
@@ -62,22 +62,22 @@ function addLaptop($id_laptop,$quantity,$price){
 
 
 //fonction qui supprime un produit
-function deleteProduct($id_laptop){
+function deleteProduct($model){
    //Si le cart existe
    if (creationcart() && !isLock())
    {
       //Creation d'un panier temporaire
       $tmp=array();
-      $tmp['id_laptop'] = array();
+      $tmp['model'] = array();
       $tmp['quantity'] = array();
       $tmp['price'] = array();
       $tmp['lock'] = $_SESSION['cart']['lock'];
 
-      for($i = 0; $i < count($_SESSION['cart']['id_laptop']); $i++)
+      for($i = 0; $i < count($_SESSION['cart']['model']); $i++)
       {
-         if ($_SESSION['cart']['id_laptop'][$i] !== $id_laptop)
+         if ($_SESSION['cart']['model'][$i] !== $model)
          {
-            array_push( $tmp['id_laptop'],$_SESSION['cart']['id_laptop'][$i]);
+            array_push( $tmp['model'],$_SESSION['cart']['model'][$i]);
             array_push( $tmp['quantity'],$_SESSION['cart']['quantity'][$i]);
             array_push( $tmp['price'],$_SESSION['cart']['price'][$i]);
          }
@@ -93,7 +93,7 @@ function deleteProduct($id_laptop){
 }
 
 //fonction qui modifie la quantite des produits.
-function modifyQtyProduct($id_laptop,$quantity){
+function modifyQtyProduct($model,$quantity){
    //Si le panier éxiste
    if (creationcart() && !isLock())
    {
@@ -101,7 +101,7 @@ function modifyQtyProduct($id_laptop,$quantity){
       if ($quantity > 0)
       {
          //Recherche du produit dans le panier
-         $searchProduct = array_search($id_laptop,  $_SESSION['cart']['id_laptop']);
+         $searchProduct = array_search($model,  $_SESSION['cart']['model']);
 
          if ($searchProduct !== false)
          {
@@ -109,7 +109,7 @@ function modifyQtyProduct($id_laptop,$quantity){
          }
       }
       else
-      deleteProduct($id_laptop);
+      deleteProduct($model);
    }
    else
    echo "We have just encounted a problem. Please contact our webmaster for more information.";
@@ -118,7 +118,7 @@ function modifyQtyProduct($id_laptop,$quantity){
 //fonction qui affiche le montant global des articles achetés.
 function totalAmount(){
    $total=0;
-   for($i = 0; $i < count($_SESSION['cart']['id_laptop']); $i++)
+   for($i = 0; $i < count($_SESSION['cart']['model']); $i++)
    {
       $total += $_SESSION['cart']['quantity'][$i] * $_SESSION['cart']['price'][$i];
    }
