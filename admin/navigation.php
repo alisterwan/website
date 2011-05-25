08af36d500fb8b59f29eb0c840720fb84aae10ac
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-25 19:40:27 +0200

    create link to accessories

--- admin/navigation.php
+++ admin/navigation.php
@@ -1,7 +1,7 @@
 <div id="nav">
 	<div>Administrator panel</div>
 	<div><a href='./newlaptop.php'>Add a laptop</a></div>
-	<div><a href='#'>Accessories</a></div>
+	<div><a href='manageacessories.php'>Accessories</a></div>
 	<div><a href='./manageproducts.php'>Manage stocks</a></div>
 	<div><a href='../logout.php'>Log out</a></div>
 </div>
