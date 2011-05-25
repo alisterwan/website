aae73db434a43279bf69af393e2865e17c92b36c
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-25 20:31:28 +0200

    add usb form correction 4

--- admin/newusb.php
+++ admin/newusb.php
@@ -21,7 +21,7 @@
 			function productForm($model,$brand,$quantity,$capacity,$price) {
 				echo "
 				<p>Here you can upload a new product, please fill in the blanks to store some information in our database.</p>
-				<form action='productpanel.php' method='post'>
+				<form action='newusb.php' method='post'>
 					<table>
 						<tr>
 							<td>Model</td>
