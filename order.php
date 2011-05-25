0c5236144fa4062c0ce3b2c1a9609a34d348cb9f
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-25 13:11:33 +0200

    filling the credit card num

---
+++ order.php
@@ -0,0 +1,10 @@
+<?php 
+echo 
+	"<p>To finalize your order, please give us additional information concerning your payment.</p>
+	<form action='./order.php' method='post'>
+	<span>Credit card number:</span>
+	<input type='text' name='creditcard'>
+	</form>";
+
+?>
+
