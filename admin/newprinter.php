5c3d53ee693c3e1b7f84ab8117ea2c111bd7a951
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-26 19:29:14 +0200

    correction

---
+++ admin/newprinter.php
@@ -0,0 +1,107 @@
+<?php
+	session_start();
+	if($_SESSION[masterpass] != '415ab40ae9b7cc4e66d6769cb2c08106e8293b48')
+		header("location: ../login.php");
+?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Add a new printer</title>
+		<meta name="description" content="Projet web">
+		<meta name="author" content="Alister & Mayhem">
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+	<body>
+
+		<?php include './navigation.php' ?>
+
+		<?php
+			function productForm($model,$brand,$type,$quantity,$price) {
+				echo "
+				<p>Here you can upload a new product, please fill in the blanks to store some information in our database.</p>
+				<form action='newcharger.php' method='post'>
+					<table>
+						<tr>
+							<td>Model</td>
+							<td><input type='text' name='model' value='$model'></td>
+						</tr>
+						<tr>
+							<td>Brand</td>
+							<td><input type='text' name='brand' value='$brand'></td>
+						</tr>
+						<tr>
+							<td>Type</td>
+							<td><input type='text' name='type' value='$type'></td>
+						</tr>
+						<tr>
+							<td>Quantity</td>
+							<td><input type='text' name='quantity' value='$quantity'></td>
+						</tr>
+						<tr>
+							<td>Price</td>
+							<td><input type='text' name='price' value='$price'></td>
+						</tr>
+						<tr>
+							<td><input type='submit' name='proceed' value='submit'></td>
+						<tr>
+					</table>
+				</form>";
+			}
+
+			if ($_POST) {
+
+				$model			= $_POST[model];
+				$brand			= $_POST[brand];
+				$type			= $_POST[type];
+				$price			= $_POST[price];
+				$quantity		= $_POST[quantity];
+	
+				
+
+				if (!$model || !$brand  || !$price  || !$quantity || !$type) {
+					echo "<span class='error'>Form incomplete, please fill it completely.</span>";
+					return productForm($model,$brand,$type,$quantity,$price);
+				}
+
+				//Connexion à la base de donnée
+				$conn = pg_connect("host=sqletud.univ-mlv.fr port=5432 dbname=jwankutk_db user=jwankutk password=Tqeouoe8");
+				if (!$conn) {
+					echo "<span class='error'>Connexion error.</span>";
+					return productForm($model,$brand,$type,$quantity,$price);
+				}
+
+				//Verification si le modele du produit est deja dans la base de donnée
+				$result = pg_query($conn,"SELECT model from printers where model='$model'");
+				if (pg_num_rows($result)) {
+					echo "This model is already in our book. Please just check the stock.";
+					return productForm($model,$brand,$type,$quantity,$price);
+				}
+
+				//Verification si le prix ou la quantité est au format numérique
+				if (!is_numeric($price) || !is_numeric($quantity)) {
+					echo "<span class='error'>Price or quantity incorrect.</span>";
+					return productForm($model,$brand,$type,$quantity,$price);
+				}
+
+				// Ajout d'un nouveau produit dans la base de donnée
+				$req = pg_query($conn,"INSERT INTO printers VALUES ('$model','$brand','$type','$quantity','$price')");
+				if (!$req) {
+					echo "<span class='error'>Query error.</span>";
+					return productForm($model,$brand,$type,$quantity,$price);
+				}
+				else {
+					echo "You have successfully uploaded this product.<br>";
+					echo "<a href='./index.php'>Click here</a>";
+				}
+			}
+
+			else {
+				productForm('','','','','');
+			}
+		?>
+
+	</body>
+</html>
+
