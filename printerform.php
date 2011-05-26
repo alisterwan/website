d865aa0d5c4adfd02c5fa05f93bdc9ced87d053b
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-26 21:28:00 +0200

    print the printer form

---
+++ printerform.php
@@ -0,0 +1,42 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Product page</title>
+		<meta name="description" content="Projet web">
+		<meta name="author" content="Alister & Mayhem">
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+	<body>
+		<?php include './header.php' ?>
+
+		<div id="body">
+			<?php
+				$id = $_GET[id];
+				//Connexion & requete
+				$conn = pg_connect("host=sqletud.univ-mlv.fr port=5432 dbname=jwankutk_db user=jwankutk password=Tqeouoe8");
+				$result = pg_query($conn,"SELECT * FROM printers WHERE id_printer='$id'");
+				$i = pg_fetch_row($result);
+
+				echo "
+		<div id='product'>
+			<div class='title'>$i[1] $i[0]</div>
+			<div class='product'>
+				<div id='descriptions'>
+					<div><strong>Type</strong>: $i[2]</div>
+					<div><strong>Quantity</strong>: $i[3]</div>
+					<div><strong>Price</strong>: $i[4] €</div>
+					<div><a href='cart.php?add=$i[5]'><img src='./add.png' width='30'><img src='./cart.png'></a></div>
+				</div>
+				<div id='picture'><img src='./Printers/$i[1]/$i[0].png'></div>
+			</div>
+		</div>";
+			?>
+		</div>
+
+		<?php include './footer.php' ?>
+	</body>
+</html>
+
