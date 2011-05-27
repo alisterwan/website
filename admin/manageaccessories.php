5fc611e5b11fa9e65b5bcad91dc28f66a3c59de3
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-27 21:14:05 +0200

    creating link to case

--- admin/manageaccessories.php
+++ admin/manageaccessories.php
@@ -23,6 +23,7 @@
 	<a href='./newusb.php'>Add a USB device</a><br/>
 	<a href='./newcharger.php'>Add a universal laptop charger</a><br/>
 	<a href='./newprinter.php'>Add a new printer</a>
+	<a href='./newcase.php'>Add a new laptop case</a>
 	
 	</body>
 </html>
