fc26fc7c8efa73985c0f2780cd42baaa1127c810
John Wan <alisterwan@gmail.com>
2011-05-27 19:20:05 +0200

    description info

--- usbform.php
+++ usbform.php
@@ -28,7 +28,8 @@
 					<div><strong>Capacity</strong>: $i[3] GB</div>
 					<div><strong>Quantity</strong>: $i[2]</div>
 					<div><strong>Price</strong>: $i[4] €</div>
-					<div><a href='cart.php?add=$i[5]'><img src='./add.png' width='30'><img src='./cart.png'></a></div>
+					<div><strong>Description</strong>: $i[5] </div>
+					<div><a href='cart.php?add=$i[6]'><img src='./add.png' width='30'><img src='./cart.png'></a></div>
 				</div>
 				<div id='picture'><img src='./USB/$i[1]/$i[0].png'></div>
 			</div>
