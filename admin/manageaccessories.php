79446fd270e096d8fbbbef79d2d1a06eced9a4a8
John Wan Kut Kai <alisterwan@gmail.com>
2011-05-27 21:17:42 +0200

    creating link to case correct

--- admin/manageaccessories.php
+++ admin/manageaccessories.php
@@ -22,7 +22,7 @@
 
 	<a href='./newusb.php'>Add a USB device</a><br/>
 	<a href='./newcharger.php'>Add a universal laptop charger</a><br/>
-	<a href='./newprinter.php'>Add a new printer</a>
+	<a href='./newprinter.php'>Add a new printer</a><br/>
 	<a href='./newcase.php'>Add a new laptop case</a>
 	
 	</body>
