a08964675b24d727e5a0c42928307c9ef078b35f
John Wan <alisterwan@gmail.com>
2011-05-27 18:52:57 +0200

    adding description field

--- admin/newcharger.php
+++ admin/newcharger.php
@@ -18,7 +18,7 @@
 		<?php include './navigation.php' ?>
 
 		<?php
-			function productForm($model,$brand,$quantity,$capacity,$price) {
+			function productForm($model,$brand,$quantity,$price,$description) {
 				echo "
 				<p>Here you can upload a new product, please fill in the blanks to store some information in our database.</p>
 				<form action='newcharger.php' method='post'>
@@ -40,6 +40,10 @@
 							<td><input type='text' name='price' value='$price'></td>
 						</tr>
 						<tr>
+							<td>Description</td>
+							<td><textarea name='details' value='$description'></textarea></td>
+						</tr>
+						<tr>
 							<td><input type='submit' name='proceed' value='submit'></td>
 						<tr>
 					</table>
@@ -52,39 +56,40 @@
 				$brand			= $_POST[brand];
 				$price			= $_POST[price];
 				$quantity		= $_POST[quantity];
+				$description	= $_POST[details];
 	
 				
 
-				if (!$model || !$brand  || !$price  || !$quantity) {
+				if (!$model || !$brand  || !$price  || !$quantity || !$description) {
 					echo "<span class='error'>Form incomplete, please fill it completely.</span>";
-					return productForm($model,$brand,$quantity,$price);
+					return productForm($model,$brand,$quantity,$price,$description);
 				}
 
 				//Connexion à la base de donnée
 				$conn = pg_connect("host=sqletud.univ-mlv.fr port=5432 dbname=jwankutk_db user=jwankutk password=Tqeouoe8");
 				if (!$conn) {
 					echo "<span class='error'>Connexion error.</span>";
-					return productForm($model,$brand,$quantity,$price);
+					return productForm($model,$brand,$quantity,$price,$description);
 				}
 
 				//Verification si le modele du produit est deja dans la base de donnée
 				$result = pg_query($conn,"SELECT model from chargers where model='$model'");
 				if (pg_num_rows($result)) {
 					echo "This model is already in our book. Please just check the stock.";
-					return productForm($model,$brand,$quantity,$price);
+					return productForm($model,$brand,$quantity,$price,$description);
 				}
 
 				//Verification si le prix ou la quantité est au format numérique
 				if (!is_numeric($price) || !is_numeric($quantity)) {
 					echo "<span class='error'>Price or quantity incorrect.</span>";
-					return productForm($model,$brand,$quantity,$price);
+					return productForm($model,$brand,$quantity,$price,$description);
 				}
 
 				// Ajout d'un nouveau produit dans la base de donnée
-				$req = pg_query($conn,"INSERT INTO chargers VALUES ('$model','$brand','$quantity','$price')");
+				$req = pg_query($conn,"INSERT INTO chargers VALUES ('$model','$brand','$quantity','$price','$description')");
 				if (!$req) {
 					echo "<span class='error'>Query error.</span>";
-					return productForm($model,$brand,$quantity,$price);
+					return productForm($model,$brand,$quantity,$price,$description);
 				}
 				else {
 					echo "You have successfully uploaded this product.<br>";
