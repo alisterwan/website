7ebe5210a04241d2374660763724fe61d9254bc3
John Wan <alisterwan@gmail.com>
2011-05-28 14:55:44 +0200

    got to replace it correctly

--- aboutus.php
+++ aboutus.php
@@ -19,6 +19,17 @@
 <div id="aboutus">Laptop de Marne la Vallee © is a company founded in May 2011 based on a commercial website project for the second semester. This website is specialized on differents kind of laptops selling, computer's accessories: Universal Laptop Charger, USB devices, Laptop Cases and also Printers.
   No other company offers as complete a technology product such as Laptop de Marne la Vallee. We provide infrastructure and business offerings that span from handheld devices to some of the world's most powerful supercomputer distribution. We offer consumers a wide range of products and services from Laptop to accessories and from computing to home printing. This comprehensive portfolio helps us match the right products, services and solutions to our customers' specific needs.</div>    
 		</div>
+        
+ <div class="names">
+ <h1 style="color:blue">John WAN KUT KAI: Co-Founder</h1>
+John WAN KUT KAI is the co-founder of Laptop de Marne la Vallee ©, a website project for IUT of Marne la Vallee. Laptop de Marne la Vallee has been named a must-read site by the DUT info 1 and is ranked as the most influential media outlet.
+
+John founded Laptop de Marne la Vallee in 2011 with his mate Nicolas Stepien. Since then, Laptop de Marne la Vallee quickly grew to be one of the top 10 and most profitable sites in the University.
+
+John was named one of the most student influencers. He was also named the "Best goal scorer" of the year by the DUT info 1 in 2011.
+
+John is based in Tournan en Brie and divides his time between France and Mauritius Island.
+ </div>       
 
 		<?php include './footer.php' ?>
 	</body>
