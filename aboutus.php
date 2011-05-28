afb980620a40a43be28f66536ecbfbebbe65df44
John Wan <alisterwan@gmail.com>
2011-05-28 11:34:25 +0200

    adding about us page

---
+++ aboutus.php
@@ -0,0 +1,24 @@
+<?php session_start(); ?>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>About us</title>
+		<meta name="description" content="Projet web">
+		<meta name="author" content="Alister & Mayhem">
+		<link rel="stylesheet" href="stylesheet.css">
+	</head>
+	<body>
+		<?php include './header.php' ?>
+
+<h1>About us:</h1>
+
+		<div id="body">
+<p>Laptop de Marne la Vallee © is a company founded in May 2011 based on a commercial website project for the second semester. This website is specialized on differents kind of laptops selling, computer's accessories: Universal Laptop Charger, USB devices, Laptop Cases and also Printers.
+  No other company offers as complete a technology product such as Laptop de Marne la Vallee. We provide infrastructure and business offerings that span from handheld devices to some of the world's most powerful supercomputer distribution. We offer consumers a wide range of products and services from Laptop to accessories and from computing to home printing. This comprehensive portfolio helps us match the right products, services and solutions to our customers' specific needs.</p>    
+		</div>
+
+		<?php include './footer.php' ?>
+	</body>
+</html>
