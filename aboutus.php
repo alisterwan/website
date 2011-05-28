dbd07da1459b07db40e099dfd7ff12f04fcc502b
John Wan <alisterwan@gmail.com>
2011-05-28 12:26:33 +0200

    adding about us page corect

--- aboutus.php
+++ aboutus.php
@@ -12,11 +12,12 @@
 	<body>
 		<?php include './header.php' ?>
 
-<h1>About us:</h1>
+
 
 		<div id="body">
-<p>Laptop de Marne la Vallee © is a company founded in May 2011 based on a commercial website project for the second semester. This website is specialized on differents kind of laptops selling, computer's accessories: Universal Laptop Charger, USB devices, Laptop Cases and also Printers.
-  No other company offers as complete a technology product such as Laptop de Marne la Vallee. We provide infrastructure and business offerings that span from handheld devices to some of the world's most powerful supercomputer distribution. We offer consumers a wide range of products and services from Laptop to accessories and from computing to home printing. This comprehensive portfolio helps us match the right products, services and solutions to our customers' specific needs.</p>    
+<h1 style="color:green">About us</h1>        
+<div id="aboutus">Laptop de Marne la Vallee © is a company founded in May 2011 based on a commercial website project for the second semester. This website is specialized on differents kind of laptops selling, computer's accessories: Universal Laptop Charger, USB devices, Laptop Cases and also Printers.
+  No other company offers as complete a technology product such as Laptop de Marne la Vallee. We provide infrastructure and business offerings that span from handheld devices to some of the world's most powerful supercomputer distribution. We offer consumers a wide range of products and services from Laptop to accessories and from computing to home printing. This comprehensive portfolio helps us match the right products, services and solutions to our customers' specific needs.</div>    
 		</div>
 
 		<?php include './footer.php' ?>
