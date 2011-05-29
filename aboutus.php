dc75294c39559fcd3ffe4031dd28fed464d0814d
John Wan <alisterwan@gmail.com>
2011-05-29 23:45:16 +0200

    adding border to contact info

--- aboutus.php
+++ aboutus.php
@@ -18,7 +18,7 @@
 				<p>Laptop de Marne la Vallee © is a company founded in May 2011 based on a commercial website project for the second semester. This website is specialized on differents kind of laptops selling, computer's accessories: Universal Laptop Charger, USB devices, Laptop Cases and also Printers.
 				No other company offers as complete a technology product such as Laptop de Marne la Vallee. We provide infrastructure and business offerings that span from handheld devices to some of the world's most powerful supercomputer distribution. We offer consumers a wide range of products and services from Laptop to accessories and from computing to home printing. This comprehensive portfolio helps us match the right products, services and solutions to our customers' specific needs.</p>
  <p>Are you searching for additional information? Please contact us:
- <p style="border:groove">contact:laptopmlv@gmail.com<br/>
+ <p id="contact">contact:laptopmlv@gmail.com<br/>
  2 Rue Albert Einstein,<br/>
  Institute of Marne la Vallee,<br/>
  Champs-sur-marne.</p>
@@ -27,7 +27,7 @@
 
 			<div class="col">
 				<h3>John WAN KUT KAI: Co-Founder</h3>
-				<p>John WAN KUT KAI is the co-founder of Laptop de Marne la Vallee ©, a website project for the Institute of Marne la Vallee. Laptop de Marne la Vallee has been named a must-read site by the DUT info 1 and is ranked as the most influential media outlet.
+				<p>John WAN KUT KAI is the co-founder of Laptop de Marne la Vallee ©, a website project for the University Institutes for Technology. Laptop de Marne la Vallee has been named a must-read site by the DUT info 1 and is ranked as the most influential media outlet.
 				John founded Laptop de Marne la Vallee in 2011 with his mate Nicolas Stepien. Since then, Laptop de Marne la Vallee quickly grew to be one of the top 10 and most profitable sites in the University.
 				John was named one of the most student influencers. He was also named the "Best goal scorer" of the year by the DUT info 1 in 2011.
 				John is based in Tournan en Brie and divides his time between France and Mauritius Island.</p>
