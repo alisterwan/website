a6d34686eb934150f005393e0bc95f628040d7e6
Nicolas Stepien <mayhemydg@gmail.com>
2011-05-31 19:18:09 +0200

    Clean view orders

--- admin/orders.php
+++ admin/orders.php
@@ -19,55 +19,43 @@
 
 		<?php
 			function form($conn,$id) {
-			$query = pg_query($conn,"SELECT * FROM orders WHERE time=$id");
-			while ($order = pg_fetch_row($query)) {
-				$quantity=$order[2];
-				$total=$order[3];
-				$table=$order[5]; 
-				
-			$res  = pg_query($conn,"SELECT username FROM customers WHERE 
-			id_customer = '$order[1]'");	
-			while ($customer = pg_fetch_row($res)){
-			$name=$customer[0];
+				echo "<p>Here is the summary of this transaction.</p>";
+				$query = pg_query($conn,"SELECT * FROM orders WHERE time=$id");
+				$order = pg_fetch_row($query);
+				$customer = pg_fetch_row(pg_query($conn,"SELECT * FROM customers WHERE id_customer='$order[1]'"));
+				echo "<p>
+					<strong>$customer[0] $customer[1]</strong> (<a href='mailto:$customer[7]'>$customer[5]</a>)<br>
+					$customer[2], $customer[3]<br>$customer[4]
+				</p>";
+
+				do {
+					$prod = pg_fetch_row(pg_query($conn,"SELECT brand, model FROM $order[5] WHERE id=$order[0]"));
+					echo "<p>
+							Model: $prod[0] $prod[1]<br>
+							Quantity: $order[2]<br>
+							Total: $order[3] €
+						</p>";
+				} while ($order = pg_fetch_row($query));
+
 			}
-			
-			$res1 = pg_query($conn,"SELECT model FROM $table WHERE 
-			id = '$order[0]'");	
-			while ($prod = pg_fetch_row($res1)){
-			$model=$prod[0];
-				
-				}
-					
-			echo "Here is the summary of this transaction. <br/> <br/>";
-			
-			
-			echo "model:$model <br/>";
-			echo "customer:$name <br/>";
-			echo "customer id:$order[1] <br/>";
-			echo "quantity:$quantity <br/>";
-			echo "Total:$total €";
-			}
-			}
-			
+
 			//Connexion à la base de donnée
 			$conn = pg_connect("host=sqletud.univ-mlv.fr port=5432 dbname=jwankutk_db user=jwankutk password=Tqeouoe8");
-			echo "<p>Select an order to view in the drop-down list.</p>";
-
-			echo "<form action='./orders.php' method='post'>
-				<button type='submit' name='select' value='customer'>Select</button>
-				<select name='choice'>";
+			echo "<p>Select an order to view in the drop-down list.</p>
+					<form action='./orders.php' method='post'>
+						<button type='submit' name='select' value='true'>Select</button>
+						<select name='choice'>";
 			$query = pg_query($conn,"SELECT time FROM orders");
-			while ($order = pg_fetch_row($query)) {
+			while ($order = pg_fetch_row($query))
 				if($order[0] != $old) {
 					echo "<option value='$order[0]'>$order[0]</option>";
 					$old = $order[0];
 				}
-			}
 			echo "</select></form>";
 
 			if ($_POST[select]) {
-				$id = $_POST[choice];
-				form($conn,$id);
+				$time = $_POST[choice];
+				form($conn,$time);
 			}
 		?>
 
