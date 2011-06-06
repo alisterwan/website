f13b44050687add1794d098231520fb98ce35845
John Wan <alisterwan@gmail.com>
2011-06-06 13:50:14 +0200

    commiting laptopcases

--- laptopcases.php
+++ laptopcases.php
@@ -8,12 +8,15 @@
 </div>
 
 <?php
+	//fonction qui charge en donnée les chargeurs.
 	function printCase($brand,$conn) {
+		
 		$result = pg_query($conn,"SELECT id, model FROM laptopcase WHERE brand='$brand'");
 
 		if (pg_num_rows($result)) {
 			echo "<div class='type'><div class='title'>$brand</div>";
 			while ($i = pg_fetch_row($result))
+		//lien qui recupere l'id laptopcase et le modele grace à la méthode GET et 		          la redirige vers une autre page pour y charger les données.
 				echo "	<a class='unit' href='./laptopcase.php?id=$i[0]'>
 							<img src='./Case/$brand/$i[1].png'>
 							<div>$i[1]</div>
