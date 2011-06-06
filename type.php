be2114352d0ec6fe40591888ad83be27511ec9c1
John Wan <alisterwan@gmail.com>
2011-06-06 22:10:29 +0200

    commiting type

--- type.php
+++ type.php
@@ -2,12 +2,16 @@
 	include './header.php';
 	printHeader($_GET[type]);
 
+	//Affichage par type de laptop.
 	function printLaptop($type,$brand,$conn) {
+		
+		//on recupere le libellé,modele du laptop correspondant.
 		$result = pg_query($conn,"SELECT id, model FROM laptop WHERE type='$type' and brand='$brand'");
 
 		if (pg_num_rows($result)) {
 			echo "<div class='type'><div class='title'>$brand</div>";
 			while ($i = pg_fetch_row($result))
+				//affiche le lien & image.
 				echo "<a class='unit' href='./laptop.php?id=$i[0]'>
 						<img src='./$brand/$type/$i[1].png'>
 						<div>$i[1]</div>
