bf3600c68c0e04c7e2a308172c36816f1c77df38
Nicolas Stepien <mayhemydg@gmail.com>
2011-06-08 14:02:51 +0200

    js

--- index.php
+++ index.php
@@ -2,40 +2,39 @@
 	include './header.php';
 	printHeader('Home Page');
 ?>
-<head>
 
-<script type="text/javaScript">
+<div id="js">
+	<img class="visible" src="./javascript/pic1.jpg"/>
+	<img class="hidden" src="./javascript/pic2.jpg"/>
+</div>
+<script>
+(function() {
+	function rotate() {
+		first = document.getElementsByClassName('visible')[0];
+		second = document.getElementsByClassName('hidden')[0];
+		cpt++;
+		if(cpt>=imgs.length)
+			cpt=0;
+		second.src=imgs[cpt];
+		second.className='visible';
+		first.className='hidden';
+	}
+	var imgs=new Array();
+	imgs[0]="./javascript/pic1.jpg";
+	imgs[1]="./javascript/pic2.jpg";
+	imgs[2]="./javascript/pic3.jpg";
+	imgs[3]="./javascript/pic4.jpg";
+	imgs[4]="./javascript/pic5.jpg";
+	var cpt=0;
+	window.setInterval(rotate, 2500);
 
-var imgs=new Array();
-imgs[0]="./javascript/pic1.jpg";
-imgs[1]="./javascript/pic2.jpg";
-imgs[2]="./javascript/pic3.jpg";
-imgs[3]="./javascript/pic4.jpg";
-imgs[4]="./javascript/pic5.jpg";
-var cpt=0;
-
-function changeimages()
-{
-	document.getElementById("javascript").src=imgs[cpt];
-	cpt++;
-	if(cpt>=imgs.length) cpt=0;
-	setTimeout("changeimages()",1500);
-}
+}).call(this);
 </script>
-</head>
 
 <p>Welcome to our site.
 Do you have difficulties to find a laptop?
 Or are you searching for a gift?<br>
 Here is where you will find all your answers.
-
-<BODY onLoad="changeimages()"> 
-<img id="javascript" name="picture" src="./javascript/pic1.jpg"/> 
-</BODY> 
-
-
-
-
 
 <?php
 	//si personne n'est connecté
