c04bc4bc7cc969d4891eccf71bacc09cab400a98
John Wan <alisterwan@gmail.com>
2011-06-15 00:22:09 +0200

    replacing the *

--- usb.php
+++ usb.php
@@ -3,7 +3,7 @@
 	printHeader('Product page');
 
 	// On affiche les données de la bdd correspondant à l'identifiant du produit recupéré grâce à la méthode get.
-	$i = pg_fetch_row(pg_query($conn,"SELECT * FROM usb WHERE id='$_GET[id]'"));
+	$i = pg_fetch_row(pg_query($conn,"SELECT model,brand,quantity,capacity,price,description,id FROM usb WHERE id='$_GET[id]'"));
 
 	echo "
 		<div id='product'>
