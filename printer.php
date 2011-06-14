bf67e01b5f3b58578272519668df40b78bc60e40
John Wan <alisterwan@gmail.com>
2011-06-15 00:19:09 +0200

    replacing the *

--- printer.php
+++ printer.php
@@ -3,7 +3,7 @@
 	printHeader('Product page');
 
 	// On affiche les données de la bdd correspondant à l'identifiant du produit recupéré grâce à la méthode get.
-	$i = pg_fetch_row(pg_query($conn,"SELECT * FROM printer WHERE id='$_GET[id]'"));
+	$i = pg_fetch_row(pg_query($conn,"SELECT model,brand,type,quantity,price,description,id FROM printer WHERE id='$_GET[id]'"));
 
 	echo "
 <div id='product'>
